eef79fc625008931c33327cf1fef5aeec1a4b7cc
king_ing_e <hyen4320@naver.com>
2025-07-17 10:24:29 -0400

    udpate sum-nbo

--- sum-nbo.cpp
+++ sum-nbo.cpp
@@ -2,14 +2,9 @@
 #include<stdint.h>
 #include <netinet/in.h>
 uint32_t readFile(FILE *fs){
-	uint32_t result;
-	uint32_t *a;
-	fread(a,1,4,fs);
-	//result=a;
-	//printf("%08x\n",ntohl(result));
-	printf("%s\n",a);
-	result=*a;
-	return ntohl(result);
+	uint32_t *result;
+	fread(result,1,4,fs);
+	return ntohl(*result);
 }
 
 
@@ -18,16 +13,20 @@ uint32_t readFile(FILE *fs){
 
 int main(int argc, char *argv[]){
 	FILE *fs1 = fopen(argv[1],"rb");
-	FILE *fs2 = fopen(argv[2],"rb");
-	FILE *fs3 = fopen(argv[3],"rb");
+	uint32_t result[3];
+	
+	for(int i=1; i<=3;i++){
+		FILE *fs1 = fopen(argv[i],"rb");
+		if(fs1==NULL){
+			printf("File Read Error");
+			break;
+		}
+		else{
+			result[i]=readFile(fs1);
+		}
+	}
 
-	uint32_t result1;
-	uint32_t result2;
-	uint32_t result3;
-	result1=readFile(fs1);
-	result2=readFile(fs2);
-	result3=readFile(fs3);
-	printf("%d (%08x) + %d (%08x) + %d (%08x) = %d (%08x)",result1,result1,result2,result2,result3,result3,result1+result2+result3,result1+result2+result3);
+	printf("%d (%08x) + %d (%08x) + %d (%08x) = %d (%08x)",result[1],result[1],result[2],result[2],result[3],result[3],result[1]+result[2]+result[3],result[1]+result[2]+result[3]);
 	return 0;
 }
 
